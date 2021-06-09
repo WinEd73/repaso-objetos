2671b2df71c0412e686f2432ef8b47cc6702297b
WinEd73 <edwinyankov2000@gmail.com>
2021-06-09 08:42:36 -0500

    Pendiente ejercicio 1

---
+++ src/main/java/uaslp/objetos/figuras/PoligonoRegular.java
@@ -0,0 +1,32 @@
+package uaslp.objetos.figuras;
+
+public class PoligonoRegular {
+
+    private int numeroDeLados;
+    private double lado;
+    private double area;
+
+    public PoligonoRegular(int numeroDeLados) {
+    }
+
+    public PoligonoRegular(int numeroDeLados, double lado) {
+        this.numeroDeLados = numeroDeLados;
+        this.lado = lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getArea() {
+        double apotema;
+
+        apotema = (lado/2)/Math.tan(Math.toRadians((double) 360/numeroDeLados)/2);
+        area=apotema*apotema*numeroDeLados*Math.tan(Math.PI/numeroDeLados);
+        return area;
+    }
+
+    public double getLado() {
+       return lado;
+    }
+}
