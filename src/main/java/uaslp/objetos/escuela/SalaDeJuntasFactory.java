689e0ba55c72a0ec5eb10b16f5a09b58af95c08b
WinEd73 <edwinyankov2000@gmail.com>
2021-06-10 20:20:53 -0500

    Terminado ejercicio 4

---
+++ src/main/java/uaslp/objetos/escuela/SalaDeJuntasFactory.java
@@ -0,0 +1,17 @@
+package uaslp.objetos.escuela;
+
+
+public class SalaDeJuntasFactory{
+    public static SalaDeJuntas get(String name){
+        switch (name){
+            case "Sala A":
+                return SalaDeJuntasA.getInstance();
+            case "Sala B":
+                return SalaDeJuntasB.getInstance();
+            case "Sala C":
+                return SalaDeJuntasC.getInstance();
+            default:
+                return null;
+        }
+    }
+}
